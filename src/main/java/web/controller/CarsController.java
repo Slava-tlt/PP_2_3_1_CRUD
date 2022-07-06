a8318641f4cd2eb593d8d42a2d96407f4becd4ac
Slava-tlt <cjiabug1@mail.com>
2022-07-06 13:14:56 +0400

    commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +17,11 @@ public class CarsController {
 	@GetMapping(value = "/cars")
 	public String cars(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
 		List<Car> list = new ArrayList<>();
-		list.add(new Car("Ford", "RED", 1000));
-		list.add(new Car("Ferrari", "YELLOW", 700));
-		list.add(new Car("Aston Martin", "BLACK", 500));
-		list.add(new Car("FIAT", "GREEN", 180));
-		list.add(new Car("BMW", "BLUE", 100));
+		list.add(new Car("Lada", "Blue", 2101));
+		list.add(new Car("BMV", "White", 325));
+		list.add(new Car("Mersedes", "Black", 124));
+		list.add(new Car("Mersedes", "Red", 60));
+		list.add(new Car("BMW", "Yellow", 720));
 		list = CarService.carsCount(list, allCars);
 		model.addAttribute("list", list);
 		return "car";
