32cb2ee51b82571c921bcb830237204b66f74357
Slava-tlt <cjiabug1@mail.com>
2022-07-10 14:22:34 +0400

    commit

---
+++ src/main/java/web/DAO/UserDAO.java
@@ -0,0 +1,34 @@
+package web.DAO;
+
+import org.springframework.stereotype.Component;
+import web.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDAO {
+
+    private static int User_Count;
+
+    private List<User> users;
+
+    {
+        users = new ArrayList<>();
+
+        users.add(new User(++User_Count, "Ivan", "Petrov", 35));
+        users.add(new User(++User_Count, "Sveta", "Naumova", 24));
+        users.add(new User(++User_Count, "Igor", "Ivanov", 31));
+        users.add(new User(++User_Count, "Petr", "Sidorov", 28));
+        users.add(new User(++User_Count, "Vasya", "Kulyakov", 18));
+    }
+
+    public List<User> allUsers() {
+        return users;
+    }
+
+    public User show(int id) {
+        return users.stream().filter(user -> user.getId() == id).findAny().orElse(null);
+    }
+
+}
---
+++ src/main/java/web/controller/UsersController.java
@@ -0,0 +1,37 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import web.DAO.UserDAO;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+	private final UserDAO userDAO;
+
+	public UsersController(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	@GetMapping()
+	public String allUsers(Model model){
+
+		model.addAttribute("users", userDAO.allUsers());
+
+		return "users/allUsers";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") int id, Model model){
+
+		model.addAttribute("user", userDAO.show(id));
+
+		return "users/show";
+	}
+	
+}
